a06a0443106e12b31a6de3547fbe4cc745407e80
Cecer <Chris@cecer1.com>
2014-01-09 16:49:03 +0000

    Added bare minimum for testing.

---
+++ Body/Body180.cs
@@ -0,0 +1,15 @@
+﻿using IHI.Server.Rooms.Figure;
+
+namespace IHI.Server.Plugins.ClassicFigures
+{
+    public class Body180 : Body
+    {
+        /// <summary>
+        /// The ID of this figure set.
+        /// </summary>
+        public override ushort Id
+        {
+            get { return 180; }
+        }
+    }
+}
---
+++ ClassicFigures.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using IHI.Server.Habbos;
+using IHI.Server.Rooms.Figure;
+using IHI.Server.Network;
+
+namespace IHI.Server.Plugins.ClassicFigures
+{
+    public class ClassicFigures : Plugin
+    {
+        public override string Name
+        {
+            get
+            {
+                return "ClassicFigures";
+            }
+        }
+
+        public override void Start(EventFirer eventFirer)
+        {
+            HabboFigureFactory factory = CoreManager.ServerCore.HabboFigureFactory;
+
+            factory.RegisterSet(typeof(Body180));
+        }
+    }
+}
